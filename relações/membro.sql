82655c22988f64dfffef2b0b83fa86fcd308552d
Rodrigo Martins <rodrigomartinsroger2006@gmail.com>
2025-11-04 17:32:51 +0000

    atualização

--- relações/membro.sql
+++ relações/membro.sql
@@ -6,8 +6,8 @@ CREATE TABLE membro(
         PRIMARY KEY (grupo,solista),
     --
     CONSTRAINT fk_membro_grupo
-        FOREIGN KEY(grupo), REFERENCES grupo ON DELETE CASCADE,
+        FOREIGN KEY(grupo) REFERENCES grupo ON DELETE CASCADE,
 --
     CONSTRAINT fk_membro_solista
-        FOREIGN KEY(solista), REFERENCES solista ON DELETE SET NULL,
+        FOREIGN KEY(solista) REFERENCES solista ON DELETE SET NULL,
 );

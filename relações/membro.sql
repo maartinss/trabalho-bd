0ce58d1f5d49148285c148b2ba3be7ba88a025c5
Pedro Dias <pedroguilhermedias2005@gmail.com>
2025-11-04 16:28:10 +0000

    idk

---
+++ relações/membro.sql
@@ -0,0 +1,13 @@
+CREATE TABLE membro(
+    grupo,
+    solista,
+    --
+    CONSTRAINT pk_membro
+        PRIMARY KEY (grupo,solista),
+    --
+    CONSTRAINT fk_membro_grupo
+        FOREIGN KEY(grupo), REFERENCES grupo ON DELETE CASCADE,
+--
+    CONSTRAINT fk_membro_solista
+        FOREIGN KEY(solista), REFERENCES solista ON DELETE SET NULL,
+);

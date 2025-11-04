523a2177eee136268ce64ff8f22f3a3d05e2ff1c
Pedro Dias <pedroguilhermedias2005@gmail.com>
2025-11-04 15:36:25 +0000

    utilizador finished

---
+++ entidades/utilizador.sql
@@ -0,0 +1,12 @@
+CREATE TABLE utilizador(
+    username    VARCHAR(24),
+    email       VARCHAR(36) CONSTRAINT nn_utilizador_email NOT NULL,
+    palavra_passe   VARCHAR(12),
+    data_de_nascimento DATE,
+--
+    CONSTRAINT pk_utilizador,
+      PRIMARY KEY (username),
+--
+    CONSTRAINT ck_utilizador_idade_minima
+    CHECK (data_de_nascimento <= ADD_MONTHS(CURRENT_DATE, -156)),
+)

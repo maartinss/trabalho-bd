c234325511ccec89c1922be6c1afef79efe706ef
Rodrigo Martins <rodrigomartinsroger2006@gmail.com>
2025-11-06 15:53:16 +0000

    Quase a funcionar

--- versao_final.sql
+++ versao_final.sql
@@ -10,18 +10,18 @@
 -- Comandos DROP TABLE
 --
 --
-DROP TABLE album IF EXISTS;
-DROP TABLE artista IF EXISTS;
-DROP TABLE solista IF EXISTS;
-DROP TABLE grupo IF EXISTS;
-DROP TABLE utilizador IF EXISTS;
-DROP TABLE versao IF EXISTS;
-DROP TABLE suporte_fisico IF EXISTS;
-DROP TABLE lista_personalizada IF EXISTS;
-DROP TABLE refere IF EXISTS;
-DROP TABLE possui IF EXISTS;
-DROP TABLE membro IF EXISTS;
-DROP TABLE em IF EXISTS;
+DROP TABLE album CASCADE CONSTRAINTS;
+DROP TABLE artista CASCADE CONSTRAINTS;
+DROP TABLE solista CASCADE CONSTRAINTS;
+DROP TABLE grupo CASCADE CONSTRAINTS;
+DROP TABLE utilizador CASCADE CONSTRAINTS;
+DROP TABLE versao CASCADE CONSTRAINTS;
+DROP TABLE suporte_fisico CASCADE CONSTRAINTS;
+DROP TABLE lista_personalizada CASCADE CONSTRAINTS;
+DROP TABLE refere CASCADE CONSTRAINTS;
+DROP TABLE possui CASCADE CONSTRAINTS;
+DROP TABLE membro CASCADE CONSTRAINTS;
+DROP TABLE em CASCADE CONSTRAINTS;
 --
 --
 -- Comandos CREATE TABLE
@@ -34,7 +34,6 @@ CREATE TABLE album(
     titulo  VARCHAR(36) CONSTRAINT nn_album_titulo NOT NULL,
     tipo    VARCHAR(5) CONSTRAINT nn_album_tipo NOT NULL,
     ano_lancamento  NUMBER(4) CONSTRAINT nn_album_ano_lancamento NOT NULL,
-    artista,
 --
     CONSTRAINT pk_album
         PRIMARY KEY (mbid),
@@ -48,29 +47,29 @@ CREATE TABLE album(
 --  RIA-1 O código MBID de um álbum tem de ter 36 caracteres
 --
     CONSTRAINT ck_album_mbid
-        CHECK(REGEXP_LIKE(mbid, '^[0-9a-z]+$') AND LENGTH(mbid) = 36),
---
-    CONSTRAINT fk_album_artista
-        FOREIGN KEY(artista) REFERENCES artista ON DELETE SET NULL,
+        CHECK(REGEXP_LIKE(mbid, '^[0-9a-z]+$') AND LENGTH(mbid) = 36)
 --
 );
 --
 -- Não é possível representar a RIA 10 solista AND grupo COVER artista
 --
 CREATE TABLE artista(
-    isni CHAR(16) CONSTRAINT pk_artista PRIMARY KEY(isni),
+    isni CHAR(16),
     nome VARCHAR(36) CONSTRAINT nn_artista_nome NOT NULL,
     ano_inicio NUMBER(4) CONSTRAINT nn_artista_ano_inicio NOT NULL,
+--
+    CONSTRAINT pk_artista 
+        PRIMARY KEY(isni),
 --  RIA-7 O código ISNI de um artista tem de ter 16 dígitos
 --
     CONSTRAINT ck_artista_isni
-        CHECK(REGEXP_LIKE(isni, '^[0-9]+$') AND LENGTH(isni) = 16),
+        CHECK(REGEXP_LIKE(isni, '^[0-9]+$') AND (LENGTH(isni) = 16)),
 --
     CONSTRAINT ck_artista_nome
-        CHECK(REGEXP_LIKE(nome, '^[A-Za-z]+$'))
+        CHECK(REGEXP_LIKE(nome, '^[A-Za-z]+$')),
 --
     CONSTRAINT ck_artista_ano_inicio
-        CHECK(ano_inicio BETWEEN 0 AND 2025),
+        CHECK(ano_inicio BETWEEN0 AND 2025)
 --
 );
 --
@@ -82,7 +81,7 @@ CREATE TABLE solista (
         PRIMARY KEY(isni),
 --
     CONSTRAINT fk_solista
-        FOREIGN KEY(isni) REFERENCES artista ON DELETE CASCADE,
+        FOREIGN KEY(isni) REFERENCES artista ON DELETE CASCADE
 --
 );
 --
@@ -94,7 +93,7 @@ CREATE TABLE grupo(
         PRIMARY KEY(isni),
 --
     CONSTRAINT fk_grupo_isni
-        FOREIGN KEY(isni) REFERENCES artista ON DELETE CASCADE,
+        FOREIGN KEY(isni) REFERENCES artista ON DELETE CASCADE
 --        
 );
 --
@@ -104,7 +103,6 @@ CREATE TABLE utilizador(
     email       VARCHAR(36) CONSTRAINT nn_utilizador_email NOT NULL, -- RIA-13 O endereço de email identifica univocamente o utilizador
     palavra_passe   VARCHAR(12),
     data_de_nascimento DATE,
-    favorito,
 --
     CONSTRAINT pk_utilizador,
         PRIMARY KEY (username),
@@ -117,10 +115,7 @@ CREATE TABLE utilizador(
 -- RIA 14 O utilizador só pode ter letras e dígitos
 --
     CONSTRAINT ck_utilizador_username
-        CHECK(REGEXP_LIKE(isni, '^[0-9a-zA-Z]+$')),
---
-    CONSTRAINT fk_utilizador_favorito
-        FOREIGN KEY(favorito) REFERENCES artista ON DELETE SET NULL,
+        CHECK(REGEXP_LIKE(isni, '^[0-9a-zA-Z]+$'))
 --
 );
 --
@@ -133,7 +128,7 @@ CREATE TABLE lista_personalizada(
         FOREIGN KEY(username) REFERENCES utilizador ON DELETE CASCADE,
 --
     CONSTRAINT pk_lista_personalizada
-        PRIMARY KEY(username, nome),
+        PRIMARY KEY(username, nome)
 --
 );
 --
@@ -141,19 +136,13 @@ CREATE TABLE lista_personalizada(
 CREATE TABLE versao(
     ean_13 CHAR(13) ,
     designacao VARCHAR(36) CONSTRAINT nn_versao_designacao NOT NULL,
-    mbid_album,
-    suporte_fisico,
 --
     CONSTRAINT pk_versao 
         PRIMARY KEY(ean_13),
 --  RIA-12 O código EAN-13 da versão de um álbum tem de ter 13 dígitos e ser positivo
 --
     CONSTRAINT ck_versao_ean_13
-        CHECK(REGEXP_LIKE('^[0-9]+$') AND LENGTH(ean_13) = 13), 
---
-    CONSTRAINT fk_versao_em
-        FOREIGN KEY(mbid_album,suporte_fisico) REFERENCES em(mbid,suporte_fisico) ON DELETE NO ACTION,
---
+        CHECK(REGEXP_LIKE('^[0-9]+$') AND LENGTH(ean_13) = 13)
 --
 );
 --
@@ -166,7 +155,7 @@ CREATE TABLE suporte_fisico(
 -- RIA 11 O tipo de um suporte físico tem de ser CD, vinil ou cassete
 --
     CONSTRAINT ck_suporte_fisico
-        CHECK(tipo IN ('cassete', 'cd', 'vinil')),
+        CHECK(tipo IN ('cassete', 'cd', 'vinil'))
 --
 );
 --
@@ -179,10 +168,10 @@ CREATE TABLE em(
         PRIMARY KEY(mbid, suporte_fisico),
 --
     CONSTRAINT fk_em_mbid
-        FOREIGN KEY(mbid) REFERENCES album ON DELETE NO ACTION,
+        FOREIGN KEY(mbid) REFERENCES album ON DELETE CASCADE,
 --
     CONSTRAINT fk_em_suporte_fisico
-        FOREIGN KEY(suporte_fisico) REFERENCES suporte_fisico ON DELETE NO ACTION,
+        FOREIGN KEY(suporte_fisico) REFERENCES suporte_fisico
 --
 );
 --
@@ -201,7 +190,7 @@ CREATE TABLE possui(
         FOREIGN KEY(username) REFERENCES utilizador ON DELETE CASCADE,
 --
     CONSTRAINT fk_possui_ean_13
-        FOREIGN KEY(ean_13) REFERENCES versao ON DELETE CASCADE,
+        FOREIGN KEY(ean_13) REFERENCES versao ON DELETE CASCADE
 --
 );
 --
@@ -217,11 +206,34 @@ CREATE TABLE refere(
         FOREIGN KEY(nome) REFERENCES lista_personalizada ON DELETE CASCADE,
 --
     CONSTRAINT fk_refere_mbid
-        FOREIGN KEY(mbid) REFERENCES album ON DELETE CASCADE,
+        FOREIGN KEY(mbid) REFERENCES album ON DELETE CASCADE
 --
 );
 --
 --
--- INSERT INTO
+-- ALTER TABLE
 --
 --
+ALTER TABLE album(
+    artista_isni,
+--
+    CONSTRAINT fk_album_artista
+        FOREIGN KEY(artista_isni) REFERENCES artista ON DELETE SET NULL
+);
+--
+--
+ALTER TABLE utilizador(
+    favorito,
+--
+    CONSTRAINT fk_utilizador_favorito
+        FOREIGN KEY(favorito) REFERENCES artista ON DELETE SET NULL
+);
+--
+--
+ALTER TABLE versao (
+    mbid_album,
+    suporte_fisico,
+--
+    CONSTRAINT fk_versao_em
+        FOREIGN KEY(mbid_album,suporte_fisico) REFERENCES em(mbid,suporte_fisico)
+);
